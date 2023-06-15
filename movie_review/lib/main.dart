be5b225653bf25a04ac8340dff0622ae958afe9e
Sanghun K <sanc93@me.com>
2023-06-15 18:00:34 +0900

    movie_review

---
+++ movie_review/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, home: HomePage(), // 홈페이지 보여주기
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 음식 사진 데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "category": "탑건: 매버릭",
+        "imgUrl": "https://i.ibb.co/sR32PN3/topgun.jpg",
+      },
+      {
+        "category": "마녀2",
+        "imgUrl": "https://i.ibb.co/CKMrv91/The-Witch.jpg",
+      },
+      {
+        "category": "범죄도시2",
+        "imgUrl": "https://i.ibb.co/2czdVdm/The-Outlaws.jpg",
+      },
+      {
+        "category": "헤어질 결심",
+        "imgUrl": "https://i.ibb.co/gM394CV/Decision-to-Leave.jpg",
+      },
+      {
+        "category": "브로커",
+        "imgUrl": "https://i.ibb.co/MSy1XNB/broker.jpg",
+      },
+      {
+        "category": "문폴",
+        "imgUrl": "https://i.ibb.co/4JYHHtc/Moonfall.jpg",
+      },
+    ];
+
+    // 화면에 보이는 영역
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 7,
+          title: Text(
+            'Movie Reviews',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          centerTitle: false,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.account_box,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                // do something
+              },
+            )
+          ],
+        ),
+        body: Column());
+  }
+}
