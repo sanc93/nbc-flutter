962188cfa71c64211bac5cd412d2fd79fba5e8b6
sanc93 <sanc93@me.com>
2023-06-25 20:14:01 +0900

    shazam - 3rd tab

--- shazam/lib/thirdTab.dart
+++ shazam/lib/thirdTab.dart
@@ -1,5 +1,5 @@
 // 세번째 페이지
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ThirdTab extends StatelessWidget {
   const ThirdTab({Key? key}) : super(key: key);
@@ -60,6 +60,226 @@ class ThirdTab extends StatelessWidget {
       ],
     };
 
-    return Center(child: Text('세번째 페이지'));
+    return SafeArea(
+      child: Column(
+        children: [
+          Text(
+            '차트',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            child: ListView(
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 180,
+                      color: Colors.purple[900],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.white),
+                            ),
+                            child: Text(
+                              "국가 및 도시별 차트",
+                              style: TextStyle(
+                                color: Colors.purple[900],
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "전 세계",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 8,
+                  color: Colors.grey[400],
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            '대한민국 차트',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Spacer(),
+                          Text(
+                            '모두 보기',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        ...chartData['korea']!.map((song) {
+                          // 스프레드 연산자: 리스트 등의 이터러블 안에 있는 요소를 분해한다.
+                          String imageUrl = song['imageUrl']!;
+                          String name = song['name']!;
+                          String artist = song['artist']!;
+
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.network(
+                                  imageUrl,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.29,
+                                ),
+                                Text(
+                                  name,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(artist),
+                              ],
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 8,
+                  color: Colors.grey[400],
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            '글로벌 차트',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Spacer(),
+                          Text(
+                            '모두 보기',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        ...chartData['global']!.map((song) {
+                          String imageUrl = song['imageUrl']!;
+                          String name = song['name']!;
+                          String artist = song['artist']!;
+
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.network(
+                                  imageUrl,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.29,
+                                ),
+                                Text(
+                                  name,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(artist),
+                              ],
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 8,
+                  color: Colors.grey[400],
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            '뉴욕 차트',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Spacer(),
+                          Text(
+                            '모두 보기',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        ...chartData['newyork']!.map((song) {
+                          String imageUrl = song['imageUrl']!;
+                          String name = song['name']!;
+                          String artist = song['artist']!;
+
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.network(
+                                  imageUrl,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.29,
+                                ),
+                                Text(
+                                  name,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(artist),
+                              ],
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
