6ad423c0c0ef5146f93c64e885ebfe252d1c35bb
sanc93 <sanc93@me.com>
2023-06-25 19:05:05 +0900

    shazam - 2nd tab

--- shazam/lib/secondTab.dart
+++ shazam/lib/secondTab.dart
@@ -1,11 +1,105 @@
 // 두번째 페이지
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SecondTab extends StatelessWidget {
   const SecondTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('두번째 페이지'));
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.lightBlueAccent, Colors.blueAccent],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      DefaultTabController.of(context).animateTo(0);
+                    },
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.person,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "라이브러리",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  GestureDetector(
+                    onTap: () {
+                      DefaultTabController.of(context).animateTo(2);
+                    },
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.show_chart,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "차트",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 80),
+            Text('Shazam 하려면 탭하세요',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800)),
+            SizedBox(height: 80),
+            Container(
+              width: 150,
+              height: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+                shape: BoxShape.circle,
+              ),
+              child: Image.network(
+                "https://i.ibb.co/hxNbZ8p/shazam.png",
+                color: Colors.white,
+                width: 100,
+                height: 100,
+              ),
+            ),
+            SizedBox(height: 130),
+            Container(
+              width: 50,
+              height: 50,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
