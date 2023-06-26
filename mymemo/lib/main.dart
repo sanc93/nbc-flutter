70b82c8526102ce70bb6abbb384c8a4013379ad9
sanc93 <sanc93@me.com>
2023-06-26 22:31:49 +0900

    mymemo - main / memo_service.dart

--- mymemo/lib/main.dart
+++ mymemo/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'memo_service.dart';
 
-void main() {
+late SharedPreferences prefs;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
   runApp(
     MultiProvider(
       providers: [
--- mymemo/lib/memo_service.dart
+++ mymemo/lib/memo_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'main.dart';
@@ -9,10 +11,22 @@ class Memo {
   });
 
   String content;
+
+  Map toJson() {
+    return {'content': content};
+  }
+
+  factory Memo.fromJson(json) {
+    return Memo(content: json['content']);
+  }
 }
 
 // Memo 데이터는 모두 여기서 관리
 class MemoService extends ChangeNotifier {
+  MemoService() {
+    loadMemoList();
+  }
+
   List<Memo> memoList = [
     Memo(content: '장보기 목록: 사과, 양파'), // 더미(dummy) 데이터
     Memo(content: '새 메모 22'), // 더미(dummy) 데이터
@@ -22,16 +36,41 @@ class MemoService extends ChangeNotifier {
     Memo memo = Memo(content: content);
     memoList.add(memo);
     notifyListeners(); // Consumer<MemoService>의 builder 부분을 호출해서 화면 새로고침
+    saveMemoList();
   }
 
   updateMemo({required int index, required String content}) {
     Memo memo = memoList[index];
     memo.content = content;
     notifyListeners();
+    saveMemoList();
   }
 
   deleteMemo({required int index}) {
     memoList.removeAt(index);
     notifyListeners();
+    saveMemoList();
+  }
+
+  saveMemoList() {
+    List memoJsonList = memoList.map((memo) => memo.toJson()).toList();
+    // [{"content": "1"}, {"content": "2"}]
+
+    String jsonString = jsonEncode(memoJsonList);
+    // '[{"content": "1"}, {"content": "2"}]'
+
+    prefs.setString('memoList', jsonString);
+  }
+
+  loadMemoList() {
+    String? jsonString = prefs.getString('memoList');
+    // '[{"content": "1"}, {"content": "2"}]'
+
+    if (jsonString == null) return; // null 이면 로드하지 않음
+
+    List memoJsonList = jsonDecode(jsonString);
+    // [{"content": "1"}, {"content": "2"}]
+
+    memoList = memoJsonList.map((json) => Memo.fromJson(json)).toList();
   }
 }
