88a9c127c36ba1cd12654efa9257549b22f45355
sanc93 <sanc93@me.com>
2023-06-30 20:30:24 +0900

    mymemo_admob - admob 패키지 설치

--- mymemo_admob/lib/main.dart
+++ mymemo_admob/lib/main.dart
@@ -1,125 +1,192 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'memo_service.dart';
+
+late SharedPreferences prefs;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
+  MobileAds.instance.initialize();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => MemoService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// 홈 페이지
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MemoService>(
+      builder: (context, memoService, child) {
+        // memoService로 부터 memoList 가져오기
+        List<Memo> memoList = memoService.memoList;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("mymemo"),
+          ),
+          body: memoList.isEmpty
+              ? Center(child: Text("메모를 작성해 주세요"))
+              : ListView.builder(
+                  itemCount: memoList.length, // memoList 개수 만큼 보여주기
+                  itemBuilder: (context, index) {
+                    Memo memo = memoList[index]; // index에 해당하는 memo 가져오기
+                    return ListTile(
+                      // 메모 고정 아이콘
+                      leading: IconButton(
+                        icon: Icon(CupertinoIcons.pin),
+                        onPressed: () {
+                          print('$memo : pin 클릭 됨');
+                        },
+                      ),
+                      // 메모 내용 (최대 3줄까지만 보여주도록)
+                      title: Text(
+                        memo.content,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      onTap: () {
+                        // 아이템 클릭시
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => DetailPage(
+                              index: index,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              // + 버튼 클릭시 메모 생성 및 수정 페이지로 이동
+              memoService.createMemo(content: '');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => DetailPage(
+                    index: memoService.memoList.length - 1,
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
   }
+}
+
+// 메모 생성 및 수정 페이지
+class DetailPage extends StatelessWidget {
+  DetailPage({super.key, required this.index});
+
+  final int index;
+
+  TextEditingController contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    MemoService memoService = context.read<MemoService>();
+    Memo memo = memoService.memoList[index];
+
+    contentController.text = memo.content;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // 삭제 버튼 클릭시
+              showDeleteDialog(context, memoService);
+            },
+            icon: Icon(Icons.delete),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          controller: contentController,
+          decoration: InputDecoration(
+            hintText: "메모를 입력하세요",
+            border: InputBorder.none,
+          ),
+          autofocus: true,
+          maxLines: null,
+          expands: true,
+          keyboardType: TextInputType.multiline,
+          onChanged: (value) {
+            // 텍스트필드 안의 값이 변할 때
+            memoService.updateMemo(index: index, content: value);
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void showDeleteDialog(BuildContext context, MemoService memoService) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("정말로 삭제하시겠습니까?"),
+          actions: [
+            // 취소 버튼
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("취소"),
+            ),
+            // 확인 버튼
+            TextButton(
+              onPressed: () {
+                memoService.deleteMemo(index: index);
+                Navigator.pop(context); // 팝업 닫기
+                Navigator.pop(context); // HomePage 로 가기
+              },
+              child: Text(
+                "확인",
+                style: TextStyle(color: Colors.pink),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
---
+++ mymemo_admob/lib/memo_service.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+// Memo 데이터의 형식을 정해줍니다. 추후 isPinned, updatedAt 등의 정보도 저장할 수 있습니다.
+class Memo {
+  Memo({
+    required this.content,
+  });
+
+  String content;
+
+  Map toJson() {
+    return {'content': content};
+  }
+
+  factory Memo.fromJson(json) {
+    return Memo(content: json['content']);
+  }
+}
+
+// Memo 데이터는 모두 여기서 관리
+class MemoService extends ChangeNotifier {
+  MemoService() {
+    loadMemoList();
+  }
+
+  List<Memo> memoList = [
+    Memo(content: '장보기 목록: 사과, 양파'), // 더미(dummy) 데이터
+    Memo(content: '새 메모'), // 더미(dummy) 데이터
+  ];
+
+  createMemo({required String content}) {
+    Memo memo = Memo(content: content);
+    memoList.add(memo);
+    notifyListeners(); // Consumer<MemoService>의 builder 부분을 호출해서 화면 새로고침
+    saveMemoList();
+  }
+
+  updateMemo({required int index, required String content}) {
+    Memo memo = memoList[index];
+    memo.content = content;
+    notifyListeners();
+    saveMemoList();
+  }
+
+  deleteMemo({required int index}) {
+    memoList.removeAt(index);
+    notifyListeners();
+    saveMemoList();
+  }
+
+  saveMemoList() {
+    List memoJsonList = memoList.map((memo) => memo.toJson()).toList();
+    // [{"content": "1"}, {"content": "2"}]
+
+    String jsonString = jsonEncode(memoJsonList);
+    // '[{"content": "1"}, {"content": "2"}]'
+
+    prefs.setString('memoList', jsonString);
+  }
+
+  loadMemoList() {
+    String? jsonString = prefs.getString('memoList');
+    // '[{"content": "1"}, {"content": "2"}]'
+
+    if (jsonString == null) return; // null 이면 로드하지 않음
+
+    List memoJsonList = jsonDecode(jsonString);
+    // [{"content": "1"}, {"content": "2"}]
+
+    memoList = memoJsonList.map((json) => Memo.fromJson(json)).toList();
+  }
+}
